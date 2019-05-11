86986087f1dcf0085a7ce412d6a75f1c8821aa53
gthbskate <skate.boarding@live.com.mx>
2019-05-11 13:32:37 -0500

    Template de dashboard 1.0

---
+++ helpdesk_ticket/dashboard/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def get_dashboard (request):
+    return render (request, 'index.html')
+
+
--- helpdesk_ticket/helpdesk_ticket/urls.py
+++ helpdesk_ticket/helpdesk_ticket/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from dashboard import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('dashboard/', views.get_dashboard),
+    
+   
 ]
