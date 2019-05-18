2ded3dda808a90385aecbfb0a29ba783b32d2f71
chicharo11 <45716389+chicharo11@users.noreply.github.com>
2019-05-18 12:07:44 -0500

    Update Login

--- helpdesk_ticket/helpdesk_ticket/settings.py
+++ helpdesk_ticket/helpdesk_ticket/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -77,6 +78,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'USER': 'django',
+        #'PASSWORD': 'django',
+        #'HOST': 'localhost',
+        #'PORT': '3306'
+
     }
 }
 
@@ -118,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+LOGIN_REDIRECT_URL= '/dashboard'
+LOGOUT_REDIRECT_URL = '/login'
