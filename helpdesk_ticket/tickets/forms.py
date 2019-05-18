43cee93d4be82c47118cbdedea4aa079cf2d7955
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2019-05-18 10:27:46 -0500

    Created New Ticket view

--- helpdesk_ticket/tickets/forms.py
+++ helpdesk_ticket/tickets/forms.py
@@ -6,5 +6,5 @@ class NewTicketForm(forms.ModelForm):
     """ New ticket form """
     class Meta:
         model = Ticket
-        fields = ('fk_status', 'fk_priority', 'fk_requester',
+        fields = ('subject', 'description', 'fk_status', 'fk_priority', 'fk_requester',
                   'fk_agent', 'fk_attachments')
