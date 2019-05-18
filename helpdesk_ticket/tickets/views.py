63980c149d7c5f7030e7d89c5323eae6b6517fe1
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2019-05-18 10:17:52 -0500

    Created new ticket form

--- helpdesk_ticket/tickets/views.py
+++ helpdesk_ticket/tickets/views.py
@@ -1,12 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
 from .models import Ticket
+from .forms import NewTicketForm
 
 
 class Tickets(View):
-    """ All Tickets view """
+    """ Tickets view """
 
     def get(self, request, priority):
         """ Get all tickets by priority """
         tickets = Ticket.objects.filter(fk_priority=priority)
         return render(request, '', {'tickets': tickets})
+
+    def post(self, request):
+        """ Create new Ticket """
+        form = NewTicketForm(request.POST)
+        form.save()
+        return redirect('/')
